9f67611e986a5801fcf73f185a8ee75cd2f9ab2d
Hoa Dam <hoangocdam@gmail.com>
2024-01-16 10:06:49 -0800

    create Ride class

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,25 @@
 class Ride
+  attr_reader :name,
+              :distance,
+              :loop,
+              :terrain
 
+  def initialize(input)
+    @name = input[:name]
+    @distance = input[:distance]
+    @loop = input[:loop]
+    @terrain = input[:terrain]
+  end
+
+  def loop?
+    @loop
+  end
+
+  def total_distance
+    if loop?
+      @distance
+    else
+      @distance * 2
+    end
+  end
 end
