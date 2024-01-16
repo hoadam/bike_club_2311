d5e3cb1df43651da04755f18e43ed89b8febcff8
Hoa Dam <hoangocdam@gmail.com>
2024-01-16 10:07:35 -0800

    create Biker class

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,3 +1,30 @@
 class Biker
+  attr_reader :name,
+              :max_distance,
+              :rides,
+              :acceptable_terrain
 
+  def initialize(name, max_distance)
+    @name = name
+    @max_distance = max_distance
+    @rides = {}
+    @acceptable_terrain = []
+  end
+
+  def learn_terrain!(terrain)
+    @acceptable_terrain << terrain if !@acceptable_terrain.include?(terrain)
+  end
+
+  def log_ride(ride, time)
+    return if !acceptable_terrain.include?(ride.terrain) && !ride.total_distance >= @max_distance
+
+    @rides[ride] = [] if !@rides.key?(ride)
+    @rides[ride] << time
+  end
+
+  def personal_report(ride)
+    return false if !@rides.key?(ride)
+
+    @rides[ride].min
+  end
 end
